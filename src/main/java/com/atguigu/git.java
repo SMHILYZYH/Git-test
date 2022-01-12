2b737e7dd52fc853a9b3d80df9c2c1fb3d01ec74
zyh <zyh@qq.com>
2022-01-12 18:10:53 +0800

    first commit

---
+++ src/main/java/com/atguigu/git.java
@@ -0,0 +1,13 @@
+package com.atguigu;
+
+/**
+ * @author
+ * @function
+ */
+public class git {
+
+    public static void main(String[] args) {
+
+        System.out.println("hello git!");
+    }
+}
